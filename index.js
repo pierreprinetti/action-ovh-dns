35e56ece20a5cd711557fa3294d968b451efd8ba
Pierre Prinetti <pierreprinetti@redhat.com>
2023-03-25 16:18:35 +0100

    Convert to ECMAScript module

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs/promises');
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { readFile } from 'node:fs/promises';
+import ovh from 'ovh';
 
-const zoneFile = core.getInput('zone-file');
-const zoneName = core.getInput('zone-name');
-
-var ovh = require('ovh')({
-	appKey: process.env.OVH_APP_KEY,
-	appSecret: process.env.OVH_APP_SECRET,
-	consumerKey: process.env.OVH_CONSUMER_KEY
-});
-
-fs.readFile(zoneFile, { encoding: 'utf8' }).then(zoneData => {
-	ovh.requestPromised('POST', `/domain/zone/${zoneName}/import`, { zoneFile: zoneData })
-		.then(response => {core.setOutput("response", response)})
-		.catch(err => { core.setFailed(err.message)})
-}).catch(err => { core.setFailed(err.message)})
+try {
+	const zoneName = getInput('zone-name');
+	const zoneFile = getInput('zone-file');
+	const client = ovh({
+		appKey: process.env.OVH_APP_KEY,
+		appSecret: process.env.OVH_APP_SECRET,
+		consumerKey: process.env.OVH_CONSUMER_KEY,
+	});
+	const zoneData = await readFile(zoneFile, { encoding: 'utf8' });
+	const response = await client.requestPromised('POST', `/domain/zone/${zoneName}/import`, { zoneFile: zoneData });
+	setOutput("response", response)
+} catch (err) {
+	setFailed(err.message)
+}
